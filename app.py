1a73e8d565959de8569bdc9784c862e74d8aa74f
y-me-why <chirag1918bindu@gmail.com>
2026-03-22 00:15:39 +0530

    Database successfully being created

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from application.database import db
-from application.controller import *
+from application.models import *
+# from application.controller import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placement.db'

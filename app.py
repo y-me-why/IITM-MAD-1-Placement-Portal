1ce9b1073b152b5218af2389619ec217e24e8426
y-me-why <chirag1918bindu@gmail.com>
2026-03-21 23:56:08 +0530

    imported controller

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from application.database import db
+from application.controller import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placement.db'
 db.init_app(app)
+
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)

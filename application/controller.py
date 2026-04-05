1258ff003d842004e38bccf39f4301ad6edd6215
y-me-why <chirag1918bindu@gmail.com>
2026-04-05 22:48:18 +0530

    Structured the code and added new endpoints(side note: confused between redirect(url_for) and render_template

--- application/controller.py
+++ application/controller.py
@@ -21,11 +21,80 @@ def login():
                 if this_user.password == password:
                     session["user_id"] = this_user.id
                     session["role"] = this_user.role
-                    return redirect(url_for("home"))
+                    return redirect(url_for("/home"))
                 else:
                     flash("Invalid credentials")
                     return redirect(url_for("login"))
     return render_template("login.html")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        password = request.form.get("password")
+        email = request.form.get("email")
+        role = request.form.get("role")
+        new_user = User(password=password, email=email, role=role)
+        db.session.add(new_user)
+        db.session.commit()
+        session["user_id"] = new_user.id
+        if role == "student":
+            return redirect(url_for("student_register"))
+        else:
+            return redirect(url_for("company_register"))
+    return render_template("register.html")
+
+@app.route("/student_register", methods=["POST", "GET"])
+def student_register():
+    user_id = session["user_id"]
+    this_user = User.query.filter_by(id=user_id).first()
+    if this_user.role != "student":
+        flash("You are not authorized to access this page")
+        return redirect(url_for("login"))
+    if request.method == "POST":
+        name = request.form.get("name")
+        yoe = request.form.get("yoe")
+        resume = request.form.get("resume")
+        about = request.form.get("about")
+        new_student = Student(student_id=user_id, name=name, yoe=yoe, resume=resume, about=about)
+        db.session.add(new_student)
+
+        phones = request.form.getlist('phone')
+        for phone in phones:
+            if phone.strip():
+                new_phone = StudentPhone(student_id=user_id, phone=phone)
+                db.session.add(new_phone)
+        db.session.commit()
+        return redirect(url_for("login"))
+
+@app.route("/company_registration", methods=["POST", "GET"])
+def company_registration():
+    user_id = session["user_id"]
+    this_user = User.query.filter_by(id=user_id).first()
+    if this_user.role != "company":
+        flash("You are not authorized to access this page")
+        return redirect(url_for("login"))
+    if request.method == "POST":
+        name = request.form.get("name")
+        website = request.form.get("website")
+        cin = request.form.get("cin")
+        new_company = Company(company_id=user_id, name=name, website=website, cin=cin)
+        db.session.add(new_company)
+
+        phones = request.form.getlist('phone')
+        for phone in phones:
+            if phone.strip():
+                new_phone = CompanyPhone(company_id=user_id, phone=phone)
+                db.session.add(new_phone)
+        addresses = request.form.getlist("address")
+        for address in addresses:
+            if address.strip():
+                new_address = CompanyAddress(company_id=user_id, address=address)
+                db.session.add(new_address)
+        db.session.commit()
+        return redirect(url_for("login"))
+
+
 
 @app.route("/admin_dashboard")
 def admin_dashboard():
@@ -131,71 +200,35 @@ def admin_dashboard():
         db.session.commit()
         return redirect(url_for("admin_dashboard")) 
 
-@app.route("/home/<int:user_id>")
-def home(user_id):
+
+@app.route("/home")
+def home():
+    user_id = session["user_id"]
     this_user = User.query.filter_by(id=user_id).first()
     if this_user.role == "student":
-        return render_template("student_dashboard.html", user_id = user_id)
+        return redirect(url_for("student_dashboard"))
     else:
-        return render_template("company_dashboard.html", user_id = user_id)
+        return redirect(url_for("company_dashboard"))
 
-@app.route("/register", methods=["POST", "GET"])
-def register():
-    if request.method == "POST":
-        password = request.form.get("password")
-        email = request.form.get("email")
-        role = request.form.get("role")
-        new_user = User(password=password, email=email, role=role)
-        db.session.add(new_user)
-        db.session.commit()
-        if role == "student":
-            return render_template("student_register.html", user_id = new_user.id)
-        else:
-            return render_template("company_register.html", user_id = new_user.id)
-    return render_template("register.html")
 
-@app.route("/student_register/<int:user_id>", methods=["POST", "GET"])
-def student_register(user_id):
-    if request.method == "POST":
-        name = request.form.get("name")
-        yoe = request.form.get("yoe")
-        resume = request.form.get("resume")
-        about = request.form.get("about")
-        new_student = Student(student_id=user_id, name=name, yoe=yoe, resume=resume, about=about)
-        db.session.add(new_student)
 
-        phones = request.form.getlist('phone')
-        for phone in phones:
-            if phone.strip():
-                new_phone = StudentPhone(student_id=user_id, phone=phone)
-                db.session.add(new_phone)
-        db.session.commit()
+@app.route("/company_dashboard")
+def company_dashboard():
+    user_id = session["user_id"]
+    this_company = Company.query.filter_by(id=user_id).first()
+    if this_company.is_approved == false:
+        flash("You are not approved yet")
         return redirect(url_for("login"))
+    all_drives = Drive.query.filter_by(company_id=user_id).all()
+    return render_template("company_dashboard.html", all_drives = all_drives, company_id=company_id)
 
-@app.route("/company_registration/<int:user_id>", methods=["POST", "GET"])
-def company_registration(user_id):
-    if request.method == "POST":
-        name = request.form.get("name")
-        website = request.form.get("website")
-        cin = request.form.get("cin")
-        new_company = Company(company_id=user_id, name=name, website=website, cin=cin)
-        db.session.add(new_company)
-
-        phones = request.form.getlist('phone')
-        for phone in phones:
-            if phone.strip():
-                new_phone = CompanyPhone(company_id=user_id, phone=phone)
-                db.session.add(new_phone)
-        addresses = request.form.getlist("address")
-        for address in addresses:
-            if address.strip():
-                new_address = CompanyAddress(company_id=user_id, address=address)
-                db.session.add(new_address)
-        db.session.commit()
-        return redirect(url_for("login"))
-
-@app.route("/create_drive/<int:company_id>", methods=["POST", "GET"])
-def create_drive(company_id):
+@app.route("/create_drive", methods=["POST", "GET"])
+def create_drive():
+    user_id = session["user_id"]
+    this_user = User.query.filter_by(id=user_id).first()
+    if this_user.role != "company" and this_user.is_approved != True:
+        flash("You are not authorized to access this page")
+        return redirect(url_for("login"))   
     if request.method == "POST":
         name = request.form.get("name")
         description = request.form.get("description")
@@ -204,23 +237,42 @@ def create_drive(company_id):
         new_drive = Drive(company_id=company_id, name=name, description=description, start_date=start_date, end_date=end_date)
         db.session.add(new_drive)
         db.session.commit()
-        return redirect(url_for("company_dashboard", company_id=company_id))
-    return render_template("create_drive.html", company_id=company_id)
+        return redirect(url_for("home"))
+    return render_template("create_drive.html")
 
-@app.route("/company_dashboard/<int:company_id>")
-def company_dashboard(company_id):
-    this_company = Company.query.filter_by(id=company_id).first()
-    if this_company.is_approved == false:
-        flash("You are not approved yet")
+@app.route("/view_drive/<int:drive_id>")
+def view_drive(drive_id):
+    user_id = session["user_id"]
+    this_user = User.query.filter_by(id=user_id).first()
+    this_drive = Drive.query.filter_by(id=drive_id).first()
+    if this_user.role != "company" and this_user.is_approved != True and this_user.company_id != this_drive.company_id:
+        flash("You are not authorized to access this page")
         return redirect(url_for("login"))
-    all_drives = Drive.query.filter_by(company_id=company_id).all()
-    return render_template("company_dashboard.html", all_drives = all_drives, company_id=company_id)
+    all_applications = Application.query.filter_by(drive_id=drive_id).all()
+    return render_template("view_drive.html", this_drive = this_drive, company_id=company_id, all_applications = all_applications)
 
-@app.route("student_dashboard/<int:student_id>")
-def student_dashboard(student_id):
-    this_student = Student.query.filter_by(id=student_id).first()
+
+
+
+@app.route("student_dashboard")
+def student_dashboard():
+    user_id = session["user_id"]
+    this_student = Student.query.filter_by(id=user_id).first()
     if this_student.is_blacklisted == true:
         flash("You are blacklisted")
         return redirect(url_for("login"))
     all_drives = Drive.query.all()
-    return render_template("student_dashboard.html", all_drives = all_drives, student_id=student_id)
+    return render_template("student_dashboard.html", all_drives = all_drives, student_id=student_id, applied_drives = Application.query.filter_by(student_id=student_id).all())
+
+@app.route("/apply_drive/<int:drive_id>")
+def apply_drive(drive_id):
+    user_id = session["user_id"]
+    this_user = User.query.filter_by(id=user_id).first()
+    this_drive = Drive.query.filter_by(id=drive_id).first()
+    if this_user.role != "student" and this_user.is_blacklisted != True and this_drive.status != "Upcoming" and this_user.id == Application.query.filter_by(drive_id=drive_id).filter_by(student_id=user_id).first():
+        flash("You are not authorized to access this page")
+        return redirect(url_for("login"))
+    new_application = Application(student_id=student_id, drive_id=drive_id)
+    db.session.add(new_application)
+    db.session.commit()
+    return redirect(url_for("home"))
